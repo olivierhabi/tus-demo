45aaffa6a76c5e2f703f8b3e7cd5eb190d413d2d
Olivier HABIMANA <habimanaolivier6@gmail.com>
2021-06-08 16:59:47 +0200

    add rest of the app

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 const tus = require("tus-node-server");
 const { config } = require("dotenv");
 const EVENTS = require("tus-node-server").EVENTS;
@@ -7,17 +10,43 @@ server.datastore = new tus.FileStore({
     path: '/files'
 });
 
+
+
 config()
 
-const host = process.env.HOST;
-const port = process.env.PORT;
+const app = express();
 
-server.listen({ host, port }, () => {
-  console.log(
-    `[${new Date().toLocaleTimeString()}] tus server listening at http://${host}:${port}`
+const uploadApp = express();
+uploadApp.all("*", server.handle.bind(server));
+app.use("/uploads", uploadApp);
+
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("hello server.jsss");
+});
+
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, OPTIONS, DELETE"
   );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-server.on(EVENTS.EVENT_UPLOAD_COMPLETE, (event) => {
-  console.log(`Upload complete for file ${event.file.id}`);
-});
+// app.listen(process.env.PORT);
+console.log("Server is listerning");
+
+const PORT = process.env.PORT || 1080;
+
+
+if (!module.parent) {
+  app.listen(PORT, () => console.log(`Up and running on port ${PORT}`));
+}
+module.exports = app;
